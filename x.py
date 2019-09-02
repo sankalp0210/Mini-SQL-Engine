1320c0ab6c3b030351a389c46eb1562d87476e29
sankalp0210 <asankalp9@gmail.com>
2019-09-02 13:00:40 +0530

    query parsed

--- x.py
+++ x.py
@@ -6,21 +6,25 @@ class Query():
     def __init__(self, query):
         self.tableList = []
         self.tableDict = {}
+        self.tables = {}
         self.readMetaData()
         self.identifiers = []
         self.parseQuery(query)
+        self.convertToLower()
+        self.processQuery()
 
     def readMetaData(self):
         f = open('./files/metadata.txt', 'r')
-        sw = '<begin_table>'
-        ew = '<end_table>'
         fields = []
         flag = 0
         for line in f:
-            if sw in line:
+            if '<begin_table>' in line:
                 flag += 1
-            elif ew in line:
+            elif '<end_table>' in line:
                 self.tableDict[self.tableList[-1]] = fields
+                self.tables[self.tableList[-1]] = {}
+                for j in fields:
+                    self.tables[self.tableList[-1]][j] = []
                 fields = []
                 flag = 0
             elif flag == 1:
@@ -35,12 +39,42 @@ class Query():
         self.qType = sqlparse.sql.Statement(self.query).get_type()
         l = sqlparse.sql.IdentifierList(self.query).get_identifiers()
         self.identifiers = [str(i) for i in l]
-        print(self.query)
-        print(self.qType)
-        print(l)
-        print(self.identifiers)
+    
+    def convertToLower(self):
+        lst = ['select', 'distinct', 'from']
+        for i in range(len(self.identifiers)):
+            if self.identifiers[i].lower() in lst:
+                self.identifiers[i] = self.identifiers[i].lower()
+
+    def getTables(self, tableNames):
+        for i in tableNames:
+            with open('./files/' + i + '.csv') as csvfile:
+                reader = csv.reader(csvfile)
+                for row in reader:
+                    for ind, j in enumerate(self.tableDict[i]):
+                        self.tables[i][j].append(row[ind])
+
+    def processQuery(self):
+        if self.qType != 'SELECT' or len(self.identifiers) < 4 or len(self.identifiers) > 6:
+            print("INVALID QUERY !!")
+            return
+
+        pos = 4 if self.identifiers[1] == 'distinct' else 3
+        self.distinct = True if self.identifiers[1] == 'distinct' else False
+
+        tableNames = []
+        for i in self.identifiers[pos].split(','):
+            if i.strip() in self.tableList:
+                tableNames.append(i.strip())
+            else:
+                print("INVALID TABLE NAME")
+                return
+        self.getTables(tableNames)
+        
+        if len(self.identifiers) == 4:
+            print("")
 
 if len(sys.argv) > 1:
-    q = Query(sys.argv[1:])
+    Query(sys.argv[1:])
 else:
-    print("Invalid Query")
+    print("INVALID QUERY !!")
